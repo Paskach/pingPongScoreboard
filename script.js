cecef7d0f3f59a077f58f2db33b520c6c6c397b9
Nathan Paskach <nathanpaskach@gmail.com>
2018-09-20 22:54:57 -0500

    fixed a bunch of stuff

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ var player1 = document.getElementById("player1");
 var player2 = document.getElementById("player2");
 body.onkeydown = function(e) {
   console.log(e.key);
-  if(player1.style.color != "green" && player2.style.color != "green"){
+  console.log(player1.style.color);
+  if(player1.style.color != "rgb(255, 232, 0)" && player2.style.color != "rgb(255, 232, 0)"){
   	if(e.key == "1") player1.innerHTML = parseInt(player1.innerHTML, 10) + 1;
   	if(e.key == "2") player2.innerHTML = parseInt(player2.innerHTML, 10) + 1;
   }
   if(e.key == "0") {
   	player1.innerHTML = 0;
   	player2.innerHTML = 0;
-  	player1.style.color = "black";
-  	player2.style.color = "black";
+  	player1.style.color = "white";
+  	player2.style.color = "white";
   }
  	var player1Score = parseInt(player1.innerHTML, 10);
   var player2Score = parseInt(player2.innerHTML, 10);
@@ -27,9 +28,9 @@ body.onkeydown = function(e) {
     }
   }
   if (player1Score >= 21 && player1Score > player2Score + 1) {
-  	player1.style.color = "green";
+  	player1.style.color = "#ffe800";
   }
   if (player2Score >= 21 && player2Score > player1Score + 1) {
-  	player2.style.color = "green";
+  	player2.style.color = "#ffe800";
   }
 }
